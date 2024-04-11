9a576c2777b6c5b73337286e105f2721632e01a2
Andres <77512331+Andresmup@users.noreply.github.com>
2024-04-11 23:30:15 +0000

    Add files to produce from csv

--- python-kafka-producer/json_ecommerce_sales_producer.py
+++ python-kafka-producer/json_ecommerce_sales_producer.py
@@ -7,7 +7,7 @@ from kafka import KafkaProducer
 producer = KafkaProducer(bootstrap_servers=['localhost:9092',])
 topic = 'ecommerce'
 
-df_ecommerce_sale_report = pd.read_csv("../ORIGINAL_DATASETS/ecommerce_sale_report.csv", index_col="index",low_memory=False)
+df_ecommerce_sale_report = pd.read_csv("ecommerce_sale_report.csv", index_col="index",low_memory=False)
 df_ecommerce_sample = df_ecommerce_sale_report.sample(n=10000, random_state=42)
 
 for index, row in df_ecommerce_sample.iterrows():
