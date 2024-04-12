4cde67bf3f4d5e300c9063a141f7b3a6744f93a8
Andres <77512331+Andresmup@users.noreply.github.com>
2024-04-12 18:55:09 +0000

    Change json producer

--- python-kafka-producer/json_ecommerce_sales_producer.py
+++ python-kafka-producer/json_ecommerce_sales_producer.py
@@ -5,15 +5,22 @@ import time
 from kafka import KafkaProducer
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092',])
-topic = 'ecommerce'
+topic = 'opensearch'
 
 df_ecommerce_sale_report = pd.read_csv("ecommerce_sale_report.csv", index_col="index",low_memory=False)
-df_ecommerce_sample = df_ecommerce_sale_report.sample(n=10000, random_state=42)
+df_ecommerce_sample = df_ecommerce_sale_report.sample(n=10, random_state=44)
 
 for index, row in df_ecommerce_sample.iterrows():
     data = {}
     for column, value in row.items():
+        if pd.isna(value):  # Convertir NaN a None
+            value = None
+        elif column == 'ship-postal-code':  # Convertir ship-postal-code a cadena
+            value = str(value)
         data[column] = value
         
     producer.send(topic, json.dumps(data).encode('utf-8'))
     time.sleep(0.1)
+
+
+    curl -XPUT -u 'master-user:master-user-password' 'domain-endpoint/movies/_doc/1' -d '{"director": "Burton, Tim", "genre": ["Comedy","Sci-Fi"], "year": 1996, "actor": ["Jack Nicholson","Pierce Brosnan","Sarah Jessica Parker"], "title": "Mars Attacks!"}' -H 'Content-Type: application/json'
